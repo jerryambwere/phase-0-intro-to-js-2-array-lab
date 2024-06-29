1b568f4d56eae07afc3151f27f2a9f775c1576d0
Melvin jerry <+>
2024-06-29 17:01:05 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Write your solution here!
+const cats= ["Milo", "Otis", "Garfield"]
+console.log(cats)
+cats()
+function destructivelyAppendCat() {
+    return cats.push('Ralph')
+}
+destructivelyAppendCat()
+
+ function destructivelyPrependCat(){
+    return cats.unshift('Bob')
+ }
+destructivelyPrependCat()
+
+function destructivelyRemoveLastCat() {
+    return cats.pop()
+}
+destructivelyRemoveLastCat()
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift()
+}
+destructivelyRemoveFirstCat()
+
+let cat;
+function appendCat() {
+   return cat=[...cats, 'Broom']
+
+}
+
+appendCat()
+
+let ncat;
+function prependCat() {
+    return ncat=['Arnold',...cats]
+    
+}
+prependCat()
+
+
+function removeLastCat() {
+    return cats.slice(0,-1)
+}
+removeLastCat()
+
+function removeFirstCat() {
+    return cats.slice(-2)
+}
+removeFirstCat()
